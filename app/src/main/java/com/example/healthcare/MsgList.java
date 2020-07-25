7d75c6ef9f01f7fd7c821d0acc1db2663ac3fd35
sriramarul <sriramarul01@gmail.com>
2020-07-25 10:57:16 +0530

    FireBase Upload Functionality added

--- app/src/main/java/com/example/healthcare/MsgList.java
+++ app/src/main/java/com/example/healthcare/MsgList.java
@@ -1,6 +1,7 @@
 package com.example.healthcare;
 
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -11,18 +12,29 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MsgList extends AppCompatActivity {
 
+    public String   smsMessageStr;
     private static MsgList inst;
     ArrayList<String> smsMessagesList = new ArrayList<String>();
     ListView smsListView;
     ArrayAdapter arrayAdapter;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+
 
     public static MsgList instance() {
         return inst;
@@ -58,8 +70,78 @@ public class MsgList extends AppCompatActivity {
             ActivityCompat.requestPermissions(MsgList.this, new String[]{"android.permission.READ_SMS"}, REQUEST_CODE_ASK_PERMISSIONS);
         }
 
+      smsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(final AdapterView<?> parent, View view,
+                                    final int position, long id) {
+
+                String uploadMessage = (String) parent.getItemAtPosition(position);
+                final String phoneNumber = uploadMessage.split(" ")[2].trim();
+                final String dbMessage=uploadMessage.replaceAll("\n","_n");
+                AlertDialog.Builder builder
+                        = new AlertDialog
+                        .Builder(MsgList.this);
+
+                builder.setMessage("Do you want to upload ?");
+
+                builder.setTitle("Alert !");
+
+                // Set Cancelable false
+                // for when the user clicks on the outside
+                // the Dialog Box then it will remain show
+                builder.setCancelable(false);
+
+                // Set the positive button with yes name
+                // OnClickListener method is use of
+                // DialogInterface interface.
+
+                builder
+                        .setPositiveButton(
+                                "Yes",
+                                new DialogInterface
+                                        .OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                                        int which)
+                                    {
+                                        DateFormat df = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");
+                                        String date = df.format(Calendar.getInstance().getTime());
+                                        myRef.child("patientdata").child(phoneNumber).child(date).setValue(dbMessage);
+                                    }
+                                });
+
+                // Set the Negative button with No name
+                // OnClickListener method is use
+                // of DialogInterface interface.
+                builder
+                        .setNegativeButton(
+                                "No",
+                                new DialogInterface
+                                        .OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                                        int which)
+                                    {
+
+                                        // If user click no
+                                        // then dialog box is canceled.
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // Create the Alert dialog
+                AlertDialog alertDialog = builder.create();
+
+                // Show the Alert Dialog box
+                alertDialog.show();
 
 
+            }
+
+        });
 
     }
     public void refreshSmsInbox() {
@@ -70,7 +152,7 @@ public class MsgList extends AppCompatActivity {
         if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
         arrayAdapter.clear();
         do {
-            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +
+            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +" "+
                     "\n" + smsInboxCursor.getString(indexBody) + "\n";
             arrayAdapter.add(str);
         } while (smsInboxCursor.moveToNext());
@@ -90,8 +172,9 @@ public class MsgList extends AppCompatActivity {
                 smsMessage += smsMessages[i];
             }
 
-            String smsMessageStr = address + "\n";
+            smsMessageStr = address + "\n";
             smsMessageStr += smsMessage;
+
             Toast.makeText(this, smsMessageStr, Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
